a18832f38c75703ecbaaacede09f70423dc8a036
alexm <alexmicle@gmail.com>
2017-06-08 21:08:49 +0300

    views

--- src/test/java/org/fasttrackit/Automation/LoginTest.java
+++ src/test/java/org/fasttrackit/Automation/LoginTest.java
@@ -1,8 +1,8 @@
 package org.fasttrackit.Automation;
-import com.sdl.selenium.web.utils.Utils;
 import org.fasttrackit.util.TestBase;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -14,9 +14,6 @@ public class LoginTest extends TestBase{
 
     @Test
     public void validLoginTest(){
-
-
-
 
         driver.get("https://rawgit.com/sdl/Testy/master/src/test/functional/app-demo/login.html");
         WebElement username = driver.findElement(By.name("username"));
@@ -36,33 +33,34 @@ public class LoginTest extends TestBase{
     }
 
 
-    @Test
-    public void failedLoginTest(){
+    @Test(dataProvider  = "invalidUsers")
+    public void failedLoginTest(String user, String pass, String expectedMessage){
 
         driver.get("https://rawgit.com/sdl/Testy/master/src/test/functional/app-demo/login.html");
         WebElement username = driver.findElement(By.name("username"));
         WebElement passwordElement = driver.findElement(By.name("password"));
         WebElement loginBtn = driver.findElement(By.id("loginButton"));
 
-        username.sendKeys("blabla");
-        passwordElement.sendKeys("blabla");
+        username.sendKeys(user);
+        passwordElement.sendKeys(pass);
         loginBtn.click();
 
        // WebElement errorMsg = driver.findElement(By.xpath("/html/body/form/div[3]"));
         WebElement errorMsg = driver.findElement(By.className("error-msg"));
         System.out.println(errorMsg.getText());
 
-        assertThat(errorMsg.getText(), is("Invalid user or password!"));
+        assertThat(errorMsg.getText(), is(expectedMessage));
 
     }
 
-
-
-
-
-
-
-
-
+@DataProvider
+public Object[][] invalidUsers(){
+        return new Object[][]{
+                {"wrong@user", "wrong.pass", "Invalid user or password!"},
+                {"empty.pass@users","","Please enter your password!"},
+                {"","","Please enter your email!"},
+                {"","","Please enter your email!"}
+        };
+}
 
 }
